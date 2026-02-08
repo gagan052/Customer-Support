a9714187ac253ffbcf3f8c221379a1f4c9d326f5
gagan052 <gagansaini7207@gmail.com>
2026-02-09 00:00:00 +0530

    All Error Fixed Related to Embedding

--- python_backend/main.py
+++ python_backend/main.py
@@ -30,7 +30,7 @@ def get_auth_client(user: UserContext):
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://localhost:8080", "http://0.0.0.0:8080"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://localhost:8080", "http://0.0.0.0:8080" , "https://customersupport-woyk.onrender.com"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["Authorization", "Content-Type", "x-api-key"],
